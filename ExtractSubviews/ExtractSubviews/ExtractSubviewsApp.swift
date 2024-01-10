d370390e507ce81048ddb7c42b4940f09a002a1e
Leo <leo.jo@asleep.ai>
2024-01-10 17:01:39 +0900

    🎉 initial code added

---
+++ ExtractSubviews/ExtractSubviews/ExtractSubviewsApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExtractSubviewsApp.swift
+//  ExtractSubviews
+//
+//  Created by Leo on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ExtractSubviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
